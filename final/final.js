fdc0d96ccf7e94b4bf17b7ace08787a8fd975422
hafidznrg <hafidznur.hn@gmail.com>
2021-01-27 11:37:35 +0700

    menyatukan page di folder final

---
+++ final/final.js
@@ -0,0 +1,12 @@
+$(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    // console.log(scroll);
+
+
+    if (scroll>70){
+        $("#myNav").addClass("berubah");
+    }
+    else{
+        $("#myNav").removeClass("berubah");
+    }
+});
